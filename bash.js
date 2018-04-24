ca890c79a17196a3ccd57489ffcd68f490a845cb
Tiny-Sheep <lsadoun@mail.bradley.edu>
2018-04-24 12:14:17 -0500

    lamine

--- bash.js
+++ bash.js
@@ -1,25 +1,18 @@
-process.stdout.write('hello');
-process.stdout.write('prompt > ');
+const pwdCommand = require("./pwd.js")
 
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-  //check what is cmd is?
-  //if data != pwd
-  ////output an error message
-  
-  if(cmd != 'pwd'){
-    //output error
-    process.stdout.write('\nbash: '+ cmd + ': command not found');
-  }
-  if(cmd === 'pwd'){
-    //get path
-     process.stdout.write(process.cwd());   
-  }
-  //process.stdout.cwd();
-  
-  //process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-});
 
+
+
+
+// process.stdout.write('\nprompt > ');
 //
+// process.stdin.on('end', () => {
+//     process.stdout.write('end');
+// });
 
+// process.stdin.on("data", (data) => {
+// const cmd = data.toString().trim();
+// if (cmd === "pwd"){
+//   pwdCommand()
+// }
+// });
